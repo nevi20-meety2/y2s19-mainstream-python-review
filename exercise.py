b0c781a7d8330c3a21ecc6e09341e4a80e62edc3
myname21-meet <myname21@meet.mit.edu>
2019-07-28 18:16:32 -0400

    py

---
+++ exercise.py
@@ -0,0 +1,34 @@
+
+
+
+# for i in range(8):
+# 	nen = input("hello")
+# 	print (nen)
+
+# 	if nen=="Nevi":
+# 		print ("you have a great name")
+# 	elif nen=="Ilan":
+# 		print("you have an ok name")
+# 	elif nen == "Loai":
+# 		print ("i like your name")
+# 	else :
+# 		print("you suck")
+# print("hi")
+
+# age = int(input("how old R U?"))
+# while age<18:
+# 	print("too young")
+# 	age = int(input("how old R U?"))
+	
+# print("old enough")
+
+
+def nothing():
+	age = int(input("how old R U?"))
+	while age<18:
+		print("too young")
+		age = int(input("how old R U?"))
+	
+	print("old enough")
+
+nothing()
