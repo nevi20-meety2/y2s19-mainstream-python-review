bebf653d54361d51f68580f6527fa92bc8e13e1b
myname21-meet <myname21@meet.mit.edu>
2019-07-28 22:20:14 -0400

    kill me

--- part2.py
+++ part2.py
@@ -1,7 +1,15 @@
 # Part 2 of the Python Review lab.
 
 def encode(x, y):
-	pass
+	if y<=1 or y>=250  :
+		print("Invalid input: Outside range.")
+		return None
+	elif 500 >= x or x >= 1000:
+		print("Invalid input: Outside range.")
+		return None
+	else:
+		return x*y
+
 
 def decode(coded_message):
 	pass
